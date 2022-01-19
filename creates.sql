1bf5a72d66b650d44183d5f7f05703021125bf1b
sympholalumiere <mbalamatrice@gmail.com>
2022-01-19 16:31:37 +0100

    creation des controllers

--- creates.sql
+++ creates.sql
@@ -70,3 +70,51 @@ create sequence hibernate_sequence start 1 increment 1;
         id_loc int8,
         primary key (id)
     );
+
+    create table public.bailleur (
+       id int8 not null,
+        email varchar(255),
+        num_cni int4 not null,
+        num_tel varchar(255),
+        password varchar(255),
+        proffession varchar(255),
+        username varchar(255),
+        primary key (id)
+    );
+create sequence hibernate_sequence start 1 increment 1;
+
+    create table locataire (
+       id int8 not null,
+        email varchar(255),
+        num_cni int4 not null,
+        num_tel varchar(255),
+        password varchar(255),
+        proffession varchar(255),
+        username varchar(255),
+        id_loc int8,
+        primary key (id)
+    );
+
+    create table public.bailleur (
+       id int8 not null,
+        email varchar(255),
+        num_cni int4 not null,
+        num_tel varchar(255),
+        password varchar(255),
+        proffession varchar(255),
+        username varchar(255),
+        primary key (id)
+    );
+create sequence hibernate_sequence start 1 increment 1;
+
+    create table locataire (
+       id int8 not null,
+        email varchar(255),
+        num_cni int4 not null,
+        num_tel varchar(255),
+        password varchar(255),
+        proffession varchar(255),
+        username varchar(255),
+        id_loc int8,
+        primary key (id)
+    );
---
+++ src/main/java/com/API/java/Controller/LocataireController.java
@@ -0,0 +1,4 @@
+package com.API.java.Controller;
+
+public class LocataireController {
+}
